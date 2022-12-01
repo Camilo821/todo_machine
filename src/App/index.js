1a1f58513f5a01a0cf941124f06076b89c12e667
Camilo <camiloarboleda821@gmail.com>
2022-12-01 11:43:20 -0500

    capitulo_4

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import { AppUI } from './AppUI'
-import { TodoProvider } from '../TodoContext';
+// import { AppUI } from './AppUI'
+// import { TodoProvider } from '../TodoContext';
 //const Swal = require('sweetalert2')
 //const defaultTodos = [
 //  { text:"Cortar cebolla", completed: false },
@@ -12,20 +12,57 @@ import { TodoProvider } from '../TodoContext';
 //  { text:"Terminar el curso de react", completed: false}
 //];
 
-
-
-function App() {
-  
+function App({ children }) {
+  const [state, setState] = React.useState('estado compartido')
+  return(
+  <React.Fragment>
+    <TodoHeader>
+      <TodoCounter />
+      <TodoList />
+    </ TodoHeader>
+    <TodoList>
+      <TodoItem state={state}/>
+    </ TodoList>
+  </ React.Fragment>
+)}
+function TodoHeader() {
+  return(
+  <React.Fragment>
+    <TodoCounter />
+    <TodoSearch />
+  </ React.Fragment>
+    );
+}
+function TodoList({ children}) {
+  return(
+  <section className="TodoList-container" >
+    {children}
+  </ section>
+)}
+function TodoCounter() {
+  return(
+    <p>TodoCounter</p>
+)}
+function TodoSearch() {
+  return(
+    <p>TodoSearch</p>
+)}
+function TodoItem({ state }) {
+  return(
+    <p>TodoItem: { state }</p>
+)}
+//function App() {
+//  
 //  console.log('render (antes)')
 //  React.useEffect(() => {
 //    console.log('use effect')
 //  }, [totalTodos])
 //  console.log('render (luego)')
-  return (
-    <TodoProvider>
-      <AppUI/>
-    </TodoProvider>
-  );
-}
+//  return (
+//    <TodoProvider>
+//      <AppUI/>
+//    </TodoProvider>
+//  );
+//}
 // props.nombre de la propiedad o props.children para poner el hijo en el render
 export default App;
